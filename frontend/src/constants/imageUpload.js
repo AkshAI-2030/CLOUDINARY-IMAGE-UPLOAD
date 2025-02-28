6c887b15050c458db3788d1dd96373765bd23ae3
AkshAI-2030 <akshayarelli2030@gmail.com>
2025-02-28 15:55:03 +0530

    setup

---
+++ frontend/src/constants/imageUpload.js
@@ -0,0 +1,3 @@
+export const FILE_UPLOAD_URL = "http://localhost:4040/files/upload";
+export const JWT_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZXNzb24iOiJjbG91ZGluYXJ5VXBsb2FkIiwiaWF0IjoxNzQwNjY2MDkwfQ.CdFHZGOB0KvcxYPJ_FeNmZaUXoJPFuzq_1YJmIsUUV8";
